1c215b9ef8cb12e7afcc5d3143f509f8ea00e671
WFactory <wfactoryshowcase@gmail.com>
2025-12-21 03:09:05 +0700

    Fix ThirdwebProvider + set W Factory favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,29 @@
+// src/pages/_app.tsx
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
+
 import { ThirdwebProvider } from "thirdweb/react";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-{
   return (
-    <>
+    <ThirdwebProvider>
       <Head>
         <title>W FACTORY | PHYGITAL WEAR</title>
 
-        <link rel="icon" href="/favicon.ico" />
+        {/* Browser tab icon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+        {/* iOS home screen */}
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Theme color */}
         <meta name="theme-color" content="#00ff88" />
       </Head>
 
       <Component {...pageProps} />
-    </>
+    </ThirdwebProvider>
   );
-}
-
 }
