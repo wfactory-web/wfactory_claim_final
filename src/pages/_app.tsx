861a6a8f952bf9260627dff804c2a7280f98391b
WFactory <wfactoryshowcase@gmail.com>
2025-12-21 02:51:04 +0700

    Update app layout / favicon config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { ThirdwebProvider } from "thirdweb/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
+{
   return (
-    <ThirdwebProvider>
+    <>
+      <Head>
+        <title>W FACTORY | PHYGITAL WEAR</title>
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#00ff88" />
+      </Head>
+
       <Component {...pageProps} />
-    </ThirdwebProvider>
+    </>
   );
+}
+
 }
